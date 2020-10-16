3c6207576420277befad2c495f5e8a456772744d
gquibao <gquibao@gmail.com>
2020-10-16 18:39:10 -0300

    Adding classes responsible for the player's basic functions

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAim : MonoBehaviour
+    {
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+            }
+        }
+
+        private void Fire()
+        {
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(transform.position, mousePosition);
+            if(hit.transform != null)
+                Debug.Log(hit.transform.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private int _speed = 5;
+        void Update()
+        {
+            var xAxis = Input.GetAxis("Horizontal");
+            var yAxis = Input.GetAxis("Vertical");
+            var movementDirection = new Vector2(xAxis, yAxis);
+            transform.Translate(movementDirection * _speed * Time.deltaTime);
+        }
+    }
+}
